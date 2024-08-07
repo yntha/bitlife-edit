bf09a4abd638e3c422bd85e3ff980f51f5483852
yntha <126660548+yntha@users.noreply.github.com>
2024-08-07 18:36:49 -0400

    Add various options to program
    - Reworked code
    - Can now encrypt var files(MonetizationVars, etc)
    - Can now dump save game data
    - Can patch save game files given a json file containing the values to override

--- Program.cs
+++ Program.cs
@@ -2,8 +2,46 @@
 using System.Text.Json;
 using System.Text;
 using System.Reflection;
+using UnityEngine;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+using System.Collections;
+using CommandLine;
 
-public class Program {
+public class Program
+{
+    class BitLifeEditOptions
+    {
+        [Value(0, MetaName = "input", Required = true, HelpText = "The input file to process.")]
+        public required string InputFile { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "The output file to write the JSON data to.")]
+        public string? OutputFile { get; set; }
+
+        [Option('m', "mono", Required = false, HelpText = "The path to the Mono DLL files.")]
+        public string? MonoDLLPath { get; set; }
+
+        [Option('d', "decrypt", Required = false, HelpText = "Decrypt the input file.")]
+        public bool Decrypt { get; set; }
+
+        [Option('e', "encrypt", Required = false, HelpText = "Encrypt the input file.")]
+        public bool Encrypt { get; set; }
+
+        [Option('c', "cipher", Required = false, HelpText = "Overrides the default cipher key used to decrypt var files.")]
+        public string? CipherKey { get; set; }
+
+        [Option('s', "save", Required = false, HelpText = "Dump the save game data to a JSON file.")]
+        public bool Save { get; set; }
+
+        [Option('l', "load", Required = false, HelpText = "Overwrite save game data based on variables stored in a JSON file.")]
+        public bool Load { get; set; }
+
+        [Option('f', "file", Required = false, HelpText = "The JSON file to load data from.")]
+        public string? JSONFile { get; set; }
+    };
+
     private const string DEFAULT_CIPHER_KEY = "com.wtfapps.apollo16";
     private static string assemblyPath = "";
     private static readonly Dictionary<int, int> obfCharMap = new() {
@@ -18,30 +56,114 @@ public class Program {
         {0x7a, 0x61}, {0x61, 0x7a}
     };
 
-    private static object? Deserialize(byte[] inputData) {
+    // there really isnt a header for binaryformatter serialized objects, so we'll just use the first 4 bytes of the file
+    private static readonly byte[] saveGameHeader = {
+        0x00, 0x01, 0x00, 0x00
+    };
+    private static BitLifeEditOptions options;
+
+    private static object? Deserialize(byte[] inputData)
+    {
         object? deserialized = null;
 
-        try {
+        try
+        {
             using MemoryStream memoryStream = new(inputData);
 
-#pragma warning disable SYSLIB0011 // Type or member is obsolete
+#pragma warning disable SYSLIB0011
             BinaryFormatter binaryFormatter = new();
-#pragma warning restore SYSLIB0011 // Type or member is obsolete
+#pragma warning restore SYSLIB0011
 
             deserialized = binaryFormatter.Deserialize(memoryStream);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             Console.WriteLine("Deserializer Error: " + e.Message);
         }
 
         return deserialized;
     }
 
-    private static string GetDecipheredItem(string b64Item, string obfuscatedCipherKey) {
+    private static string GetCipheredItem(string item, string obfuscatedCipherKey)
+    {
+        StringBuilder cipheredItem = new();
+        int i = 0;
+
+        while (i < item.Length)
+        {
+            cipheredItem.Append((char)(obfuscatedCipherKey[i % obfuscatedCipherKey.Length] ^ item[i]));
+            i++;
+        }
+
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(cipheredItem.ToString()));
+    }
+
+    // encrypt the json file in inputFile to a var file
+    private static void EncryptVarFile()
+    {
+        string json = File.ReadAllText(options.InputFile);
+        Dictionary<string, object>? itemMap = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+
+        if (itemMap == null)
+        {
+            Console.WriteLine("Invalid JSON file.");
+            return;
+        }
+
+        string obfuscatedCipherKey = "";
+        string cK = options.CipherKey ?? DEFAULT_CIPHER_KEY;
+
+        foreach (char c in cK.ToLower())
+        {
+            int obfChar;
+
+            if (!obfCharMap.ContainsKey(c))
+            {
+                obfChar = c;
+            }
+            else
+            {
+                obfChar = obfCharMap[c];
+            }
+
+            obfuscatedCipherKey = string.Format("{0}{1}", obfuscatedCipherKey, (char)obfChar);
+        }
+
+        StringBuilder varFile = new();
+
+        foreach (var item in itemMap)
+        {
+            string cipheredKey = GetCipheredItem(item.Key, obfuscatedCipherKey);
+
+            // serialize the value to a byte array using binaryformatter
+            using MemoryStream memoryStream = new();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+            BinaryFormatter binaryFormatter = new();
+            binaryFormatter.Serialize(memoryStream, item.Value);
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+            memoryStream.Position = 0;
+
+            string serializedValue = Convert.ToBase64String(memoryStream.ToArray());
+            string cipheredValue = GetCipheredItem(serializedValue, obfuscatedCipherKey);
+
+            varFile.Append(string.Format("{0}:{1}\n", cipheredKey, cipheredValue));
+        }
+
+        string outputFile = options.OutputFile ?? options.InputFile + ".var";
+
+        File.WriteAllText(outputFile, varFile.ToString());
+
+        Console.WriteLine("Encrypted JSON file to: " + outputFile);
+    }
+
+    private static string GetDecipheredItem(string b64Item, string obfuscatedCipherKey)
+    {
         StringBuilder decipheredItem = new();
         string b64DecodedItem = Encoding.UTF8.GetString(Convert.FromBase64String(b64Item));
         int i = 0;
 
-        while (i < b64DecodedItem.Length) {
+        while (i < b64DecodedItem.Length)
+        {
             decipheredItem.Append((char)(obfuscatedCipherKey[i % obfuscatedCipherKey.Length] ^ b64DecodedItem[i]));
             i++;
         }
@@ -49,30 +171,32 @@ public class Program {
         return decipheredItem.ToString();
     }
 
-    private static string Decrypt(string inputFile) {
-        string[] fileLines = File.ReadAllLines(inputFile);
+    private static void DecryptVarFile()
+    {
+        string[] fileLines = File.ReadAllLines(options.InputFile);
         Dictionary<string, object> itemMap = [];
 
-        Console.WriteLine("Items: " + fileLines.Length);
-
         string obfuscatedCipherKey = "";
-        string cK = DEFAULT_CIPHER_KEY.ToLower();
+        string cK = options.CipherKey ?? DEFAULT_CIPHER_KEY;
 
-        foreach (char c in cK) {
+        foreach (char c in cK)
+        {
             int obfChar;
 
-            if (!obfCharMap.ContainsKey(c)) {
+            if (!obfCharMap.ContainsKey(c))
+            {
                 obfChar = c;
-            } else {
+            }
+            else
+            {
                 obfChar = obfCharMap[c];
             }
 
             obfuscatedCipherKey = string.Format("{0}{1}", obfuscatedCipherKey, (char)obfChar);
         }
 
-        foreach (string line in fileLines) {
-            Console.WriteLine("Decoding item: " + line);
-
+        foreach (string line in fileLines)
+        {
             string[] lineItems = line.Split(':');
             string key = lineItems[0];
             string value = lineItems[1];
@@ -80,62 +204,218 @@ public class Program {
             string decipheredKey = GetDecipheredItem(key, obfuscatedCipherKey);
             string decipheredValue = GetDecipheredItem(value, obfuscatedCipherKey);
 
-            Console.WriteLine("Deciphered key: " + decipheredKey);
-            Console.WriteLine("Deciphered value: " + decipheredValue.ToString() + "\n");
-
             // the deciphered value is a base64 encoded string that represents a serialized object.
             byte[] serializedData = Convert.FromBase64String(decipheredValue.ToString());
             object? deserialized = Deserialize(serializedData);
 
-            if (deserialized != null) {
+            if (deserialized != null)
+            {
                 itemMap.Add(decipheredKey.ToString(), deserialized);
             }
         }
 
-        return JsonSerializer.Serialize(itemMap, new JsonSerializerOptions { WriteIndented = true });
+        string json = JsonSerializer.Serialize(itemMap, new JsonSerializerOptions { WriteIndented = true, IncludeFields = true });
+        string outputFile = options.OutputFile ?? options.InputFile + ".json";
+
+        File.WriteAllText(outputFile, json);
+
+        Console.WriteLine("Decrypted var file to: " + outputFile);
     }
 
-    private static Assembly MonoAssemblyResolver(object? sender, ResolveEventArgs args) {
+    private static Assembly MonoAssemblyResolver(object? sender, ResolveEventArgs args)
+    {
         string? assemblyName = new AssemblyName(args.Name).Name;
-        string assemblyFilePath = Path.Combine(assemblyPath, assemblyName + ".dll");
+        string assemblyFilePath = Path.Combine(options.MonoDLLPath!, assemblyName + ".dll");
 
-        if (File.Exists(assemblyFilePath)) {
+        if (File.Exists(assemblyFilePath))
+        {
             return Assembly.LoadFrom(assemblyFilePath);
-        } else {
+        }
+        else
+        {
             throw new FileNotFoundException("Assembly not found: " + assemblyFilePath);
         }
     }
 
-    public static void Main(string[] args) {
-        string inputFile = args[0];
+    private static Life? GetDeserializedSaveGame(string inputFile)
+    {
+        if (options.MonoDLLPath == null)
+        {
+            Console.WriteLine("The Mono DLL path is required to deserialize the save game data.");
+            return null;
+        }
 
-        if (inputFile.EndsWith(".data")) {
-            Console.Write("Enter the path to the Mono DLL files: ");
-            string? path = Console.ReadLine();
+        AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(MonoAssemblyResolver);
+        return (Life?)Deserialize(File.ReadAllBytes(inputFile));
+    }
 
-            if (string.IsNullOrEmpty(path)) {
-                Console.WriteLine("Invalid path to the Mono DLL files.");
+    private static void OverwriteSaveGameValues()
+    {
+        JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            Converters = { new SaveDataJSONConverter() },
+            WriteIndented = true,
+            IncludeFields = true
+        };
+        Life? deserialized = GetDeserializedSaveGame(options.InputFile);
+
+        if (deserialized != null)
+        {
+            string json = File.ReadAllText(options.JSONFile!);
+            Dictionary<string, object>? data = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+
+            if (data == null)
+            {
+                Console.WriteLine("Invalid JSON file.");
                 return;
             }
 
-            assemblyPath = path;
+            foreach (var item in data)
+            {
+                FieldInfo? field = deserialized.GetType().GetField(item.Key);
 
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(MonoAssemblyResolver);
-
-            object? deserialized = Deserialize(File.ReadAllBytes(inputFile));
-
-            if (deserialized != null) {
-                File.WriteAllText(inputFile + ".json", JsonSerializer.Serialize(deserialized, new JsonSerializerOptions { WriteIndented = true }));
-            } else {
-                Console.WriteLine("Failed to deserialize the input file. Serializer returned null.");
+                if (field != null)
+                {
+                    field.SetValue(deserialized, item.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Field not found: " + item.Key);
+                }
             }
+
+            // serialize to a stream
+            using MemoryStream memoryStream = new();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+            BinaryFormatter binaryFormatter = new();
+            binaryFormatter.Serialize(memoryStream, data);
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+            File.WriteAllBytes(options.InputFile, memoryStream.ToArray());
+
+            Console.WriteLine("Overwrote save game data with values from: " + options.JSONFile);
+        }
+        else
+        {
+            Console.WriteLine("Failed to deserialize the input file. Serializer returned null.");
+        }
+    }
+
+    private static void DumpSaveGame()
+    {
+        JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            Converters = { new SaveDataJSONConverter() },
+            WriteIndented = true,
+            IncludeFields = true
+        };
+        Life? deserialized = GetDeserializedSaveGame(options.InputFile);
+
+        if (deserialized != null)
+        {
+            string json = JsonSerializer.Serialize(deserialized, jsonSerializerOptions);
+            string unescaped = Regex.Unescape(json);
+            string outputFile = options.OutputFile ?? options.InputFile + ".json";
+
+            File.WriteAllText(outputFile, unescaped);
+
+            Console.WriteLine("Dumped save game data to: " + outputFile);
+        }
+        else
+        {
+            Console.WriteLine("Failed to deserialize the input file. Serializer returned null.");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        options = Parser.Default.ParseArguments<BitLifeEditOptions>(args).Value;
+
+        byte[] fileHeader = File.ReadAllBytes(options.InputFile).Take(8).ToArray();
+
+        // check if this is a save game file
+        if (fileHeader.SequenceEqual(saveGameHeader))
+        {
+            Console.WriteLine("Auto-detected a save game file. Actions will be limited to dumping only.");
+
+            DumpSaveGame();
 
             return;
         }
 
-        // this is most likely one of the encrypted files. each line in the file is
-        // an encrypted key-value pair. the value, however, represents a serialized
-        // object that must be deserialized with the BinaryFormatter class.
-        File.WriteAllText(inputFile + ".json", Decrypt(inputFile));
+        if (options.Save) { DumpSaveGame(); return; }
+
+        if (options.Load) { OverwriteSaveGameValues(); return; }
+
+        if (options.Decrypt) { DecryptVarFile(); return; }
+
+        if (options.Encrypt) { EncryptVarFile(); return; }
+    }
+}
+
+public class SaveDataJSONConverter : JsonConverter<Life>
+{
+    public override Life? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, Life value, JsonSerializerOptions options)
+    {
+        var props = this.Collect(value);
+
+        var ser = JsonSerializer.Serialize(props, options);
+
+        writer.WriteStringValue(ser);
+    }
+
+    // recursively add all fields to a dictionary and return it
+    private Dictionary<string, object> Collect(object obj)
+    {
+        var props = obj.GetType()
+                             .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                             .ToDictionary(x => x.Name, x => x.GetValue(obj));
+
+        foreach (var prop in props)
+        {
+            if (prop.Value != null)
+            {
+                if (prop.Value is not IEnumerable && prop.Value.GetType().IsClass && prop.Value.GetType() != typeof(string))
+                {
+                    props[prop.Key] = Collect(prop.Value);
+                }
+                else
+                {
+                    // check if this is a collection
+                    if (prop.Value is IEnumerable enumerable && !(prop.Value is string))
+                    {
+                        Console.WriteLine("Found enumerable: " + prop.Key);
+                        Console.WriteLine("Type: " + prop.Value.GetType());
+
+                        List<object> list = new();
+
+                        foreach (var item in enumerable)
+                        {
+                            if (item is string)
+                            {
+                                list.Add(item);
+                                continue;
+                            }
+
+                            list.Add(Collect(item));
+                        }
+
+                        props[prop.Key] = list;
+
+                        continue;
+                    }
+
+                    props[prop.Key] = prop.Value;
+                }
+            }
+        }
+
+        return props;
     }
 }
